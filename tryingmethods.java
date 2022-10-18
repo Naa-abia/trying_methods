aa30d36a92e8cf55e44b06d6bcb890062880e7c5
Naa-abia <augusta.addy@meltwater.org>
2022-10-18 22:43:13 +0000

    dynamic methods created

--- tryingmethods.java
+++ tryingmethods.java
@@ -1,7 +1,17 @@
 public class TryingMethods {
 //this class is would be used to try the different methods
-static void myMethod() {
+static void Main() {
 //code to be executed
 
 }
+
+void DayOfWeek() {
+    //code to be executed
+    
+    }
+
+    void MyMessage() {
+        //code to be executed
+        
+        }    
 }
