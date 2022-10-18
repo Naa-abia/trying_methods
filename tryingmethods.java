3b045b849059a89f96b45902834fd040c669d0d3
Naa-abia <augusta.addy@meltwater.org>
2022-10-18 23:41:10 +0000

    dynamic methods-switch case

--- tryingmethods.java
+++ tryingmethods.java
@@ -5,13 +5,38 @@ static void Main() {
 
 }
 
-void DayOfWeek() {
-    //code to be executed
+//switch case used to check if the value is matched to the day of the week. Hence based on the value input , the method would return a specific day of the week. This is cleaner than writing long if statements. 
+void DayOfWeek(int day) {
+    switch (day) {
+        case 1:
+        System.out.println("Monday");
+            break;
+        case 2:
+        System.out.println("Tuesday");
+            break;
+        case 3:
+        System.out.println("Wednesday");
+            break; 
+        case 4:
+        System.out.println("Thursday");
+            break;
+        case 5:
+        System.out.println("Friday");
+            break;
+        case 6:
+        System.out.println("Saturday");
+            break;
+        case 5:
+        System.out.println("Sunday");
+            break;                         
+         
+        default:
+        System.out.println("Oops! Sorry this day doesn't existed on planet Earth.")
+            break;
+    }
     
     }
 
-    void MyMessage() {
-        //code to be executed
-        
-        }    
+   // DayOfWeek(1);
+    
 }
