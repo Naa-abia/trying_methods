5398b59c2a87e65def264fc8961204e3955a27b7
Naa-abia <augusta.addy@meltwater.org>
2022-10-19 08:36:09 +0000

    invoked dynamic class 1

--- tryingmethods.java
+++ tryingmethods.java
@@ -3,7 +3,9 @@ public class TryingMethods {
     public static void main(String[] args) {
         // code to be executed
         // DayOftheWeek(6);
-        
+        TryingMethods mainClass = new TryingMethods(); // creating instance of a class to be able to invoke a dynamic
+                                                       // method
+        System.out.println(mainClass.DayOftheWeek(6));
     }
 
     /*
@@ -11,37 +13,37 @@ public class TryingMethods {
      * Hence based on the value input , the method would return a specific day of
      * the week. This is cleaner than writing long if statements.
      */
- String DayOftheWeek(int day) {
-    String dayName = " ";
-    switch (day) {
-        case 1:
-       dayName = "Monday";
-            break;
-        case 2:
-        dayName = "Tuesday";
-            break;
-        case 3:
-        dayName ="Wednesday";
-            break; 
-        case 4:
-        dayName ="Thursday";
-            break;
-        case 5:
-        dayName ="Friday";
-            break;
-        case 6:
-        dayName ="Saturday";
-            break;
-        case 7:
-        dayName ="Sunday";
-            break;                         
-         
-        default:
-        dayName ="Oops! Sorry this day doesn't existed on planet Earth.";
-            break;       
-    }
-    
-    return dayName;
+    String DayOftheWeek(int day) {
+        String dayName = " ";
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+
+            default:
+                dayName = "Oops! Sorry this day doesn't existed on planet Earth.";
+                break;
+        }
+
+        return dayName;
 
     }
 
@@ -49,9 +51,4 @@ public class TryingMethods {
     // this is my second dynamic methods which will use a if statements to get an
     // answer of a string
 
-   
-
-   
-
-   
 }
