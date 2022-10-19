1dbc98963c4cf582b3989f11f5bca74d8366c020
Naa-abia <augusta.addy@meltwater.org>
2022-10-19 00:52:53 +0000

    a

--- tryingmethods.java
+++ tryingmethods.java
@@ -1,12 +1,14 @@
 public class TryingMethods {
-//this class is would be used to try the different methods
-static void Main() {
-//code to be executed
+    // this class is would be used to try the different methods
+    public static void main(String[] args) {
+        // code to be executed
+       // DayOftheWeek(6);
+    }
 
-}
-
-//switch case used to check if the value is matched to the day of the week. Hence based on the value input , the method would return a specific day of the week. This is cleaner than writing long if statements. 
-void DayOfWeek(int day) {
+    // switch case used to check if the value is matched to the day of the week.
+    // Hence based on the value input , the method would return a specific day of
+    // the week. This is cleaner than writing long if statements.
+ void DayOftheWeek(int day) {
     switch (day) {
         case 1:
         System.out.println("Monday");
@@ -26,17 +28,20 @@ void DayOfWeek(int day) {
         case 6:
         System.out.println("Saturday");
             break;
-        case 5:
+        case 7:
         System.out.println("Sunday");
             break;                         
          
         default:
-        System.out.println("Oops! Sorry this day doesn't existed on planet Earth.")
+        System.out.println("Oops! Sorry this day doesn't existed on planet Earth.");
             break;
     }
     
+
     }
 
-   // DayOfWeek(1);
-    
+   // DayOftheWeek(1);
+    // this is my second dynamic methods which will use a if statements to get an
+    // answer of a string
+
 }
